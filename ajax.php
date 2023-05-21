3aa4fafb1bdbb52f53e3ac02fbaccd1bea55fc23
ophidianpascal <74556877+ophidianpascal@users.noreply.github.com>
2023-05-21 13:44:14 +0400

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -5,7 +5,7 @@ $tg_bot_token = "6222018668:AAFchhDi7jUgFN9aWEl5YMyki76aYwzgcCM";
 $chat_id = "-997698514";
 
 $text = '';
-
+error_log('Отладочное сообщение: 1');
 foreach ($_POST as $key => $val) {
     $text .= $key . ": " . $val . "\n";
 }
@@ -17,7 +17,7 @@ $param = [
     "chat_id" => $chat_id,
     "text" => $text
 ];
-
+error_log('Отладочное сообщение: 2');
 $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
 
 var_dump($text);
@@ -33,7 +33,7 @@ foreach ( $_FILES as $file ) {
     $document = new \CURLFile($file['name']);
 
     $ch = curl_init();
-
+error_log('Отладочное сообщение: 3');
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
@@ -44,8 +44,8 @@ foreach ( $_FILES as $file ) {
     $out = curl_exec($ch);
 
     curl_close($ch);
-
+error_log('Отладочное сообщение: 4');
     unlink($file['name']);
 }
-
+error_log('Отладочное сообщение: 5');
 die('1');
